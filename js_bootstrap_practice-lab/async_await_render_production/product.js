e43cbee2423e1bc67851d9e1be505ad316edd0e1
tygfuidseujzx@gmail.com <tygfuidseujzx@gmail.com>
2025-06-17 18:31:10 +0800

    添加分頁切換和渲染

--- js_bootstrap_practice-lab/async_await_render_production/product.js
+++ js_bootstrap_practice-lab/async_await_render_production/product.js
@@ -16,7 +16,18 @@ function getProductTemplate({productimg, productname, productdesc, productprice}
     </div>
   `;
 }
-
+function renderpagebutton({page}){
+      return `<li class="page-item">
+                    <button  class="page-link renderpage " >${page}</button>
+                </li>`;
+    };
+    function renderpagenext(){
+      return `<li class="page-item">
+                    <button  class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </button>
+                </li>`;
+    };
 async function apirequest(url, options = { method: 'GET' }) {
     try {
         const res = await fetch(url, options);
@@ -46,7 +57,22 @@ apirequest('product.json').then(result => {
     }  */
     //renderstart.innerHTML=copy; 
     /* 第二種作法 */
-    for(let i=0;i<result.length;i++){
+    /* 計算json長度並算出頁數 */
+    
+    let pagesize=8;
+    let totalpage=Math.ceil(result.length/pagesize);
+    const pagination=document.querySelector('.pagination');
+    for(let x=0;x<totalpage;x++){
+      tem=x+1;
+      let totalpagebutton=renderpagebutton({
+        "page" : tem
+      });
+      pagination.insertAdjacentHTML('beforeend',totalpagebutton);
+    }
+    let nextbutton=renderpagenext();
+    pagination.insertAdjacentHTML('beforeend',nextbutton);
+
+    for(let i=0;i<pagesize;i++){
         copy=getProductTemplate({
         "productimg" : result[i].image,
         "productname" :result[i].pname,
@@ -55,6 +81,33 @@ apirequest('product.json').then(result => {
         });
         renderstart.insertAdjacentHTML('beforeend',copy);
     }
+    
+    let pagebutton=document.querySelectorAll(".renderpage");
+    for(let j=0;j<pagebutton.length;j++){
+      if(j==totalpage){
+        break;
+      }
+      pagebutton[j].addEventListener('click',()=>{
+        renderstart.innerHTML='';
+        
+        for(let i=0+j*pagesize;i<Math.min(pagesize+j*pagesize,result.length);i++){
+          copy=getProductTemplate({
+          "productimg" : result[i].image,
+          "productname" :result[i].pname,
+          "productdesc" :result[i].description,
+          "productprice":result[i].price
+        });
+        renderstart.insertAdjacentHTML('beforeend',copy);
+        }
+        pagebutton.forEach((item)=>{
+          item.classList.remove('active');
+        })
+        pagebutton[j].classList.add('active');
+      })
+    }
+    
+
+      
 });
 
 
